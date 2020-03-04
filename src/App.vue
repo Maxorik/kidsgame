e65fee758217d0a6ceeb90cc3b90da0d491c6a5d
Maxorik <gms441@mail.com>
2020-03-04 09:50:19 +0700

    fix styles

--- src/App.vue
+++ src/App.vue
@@ -222,18 +222,18 @@ export default {
 </script>
 
 <style lang="scss">
-    body, div, p{
+    body, div, p {
         margin: 0;
         padding: 0;
     }
     
-    %flexCenter{
+    %flexCenter {
         display: flex;
         justify-content: center;
         align-items: center;
     }
     
-    %letter{
+    %letter {
         color: beige;
         text-shadow: #000 1px 1px 0, #000 -1px -1px 0, #000 -1px 1px 0, #000 1px -1px 0;
         font-weight: bold;
@@ -243,10 +243,10 @@ export default {
     #game {
         padding-top: 20px;
         
-        .game__header{
+        .game__header {
             @extend %flexCenter;
             
-            .game__rules{
+            .game__rules {
                 width: 100%;
                 text-align: center;
                 font-size: 2em;
@@ -254,38 +254,38 @@ export default {
                 @extend %letter;
             }
             
-            .game__time{
+            .game__time {
                 position: absolute;
                 right: 30px;
                 top:20px;
                 @extend %letter;
                 font-size: 2em;
                 
-                span{
+                span {
                     color: #9c00ff;
                 }
             }
         }
         
-        .game__wordSpot{
-            width:96%;
+        .game__wordSpot {
+            width: 96%;
             border: 2px solid #fc7f1b;
             border-radius: 20px;
-            margin:auto;
+            margin: auto;
             background: beige;
             display: grid;  
         }
         
-        .game__board{
+        .game__board {
             display: grid;
             margin: auto;
             grid-template-columns: repeat(12, 5em);
             width: 12 * 5em;
         }
         
-        .game__board__letter{
+        .game__board__letter {
             font-size: 3em;
-            cursor:pointer;
+            cursor: pointer;
             padding: 10px;
             margin: 5px;
             text-align: center;
@@ -294,15 +294,15 @@ export default {
             border-radius: 10px;
             @extend %letter;
 
-            &:hover{
+            &:hover {
                 background: rgb(252,233,0);
                 background: radial-gradient(circle, rgba(252,233,0,1) 36%, rgba(251,54,0,1) 100%, rgba(255,0,0,1) 100%);
             }
         }
         
-        .game__reset{
+        .game__reset {
             font-size: 2em;
-            cursor:pointer;
+            cursor: pointer;
             padding: 10px;
             background: #9c00ff;
             text-align: center;
@@ -311,24 +311,23 @@ export default {
             @extend %letter;
             border-radius: 10px;
             
-            &:hover{
+            &:hover {
                 background: orangered;
             }
         }
         
-        .game__end{
+        .game__end {
             display: block;
             position: fixed;
-            z-index: 1;
             left: 0;
             top: 0;
             width: 100%;
             height: 100%;
             overflow: auto;
             background-color: rgba(0,0,0,0.6);
-            z-index: 1000;
+            z-index: 100;
             
-            .game__end__window{
+            .game__end__window {
                 @extend %letter;
                 font-size: 2em;
                 background-color: beige;
@@ -340,17 +339,17 @@ export default {
                 text-align: center;
                 border-radius: 10px;
                 
-                .game__end__interface{
+                .game__end__interface {
                     @extend %flexCenter;
                     width: 50%;
                     margin: auto;
                     justify-content: space-between;
                     margin-top: 30px;
                     
-                    .game__end__btn{
+                    .game__end__btn {
                         @extend %letter;
                         background: #9c00ff;
-                        font-size: 0.6em;
+                        font-size: .6em;
                         padding: 10px 20px;
                         cursor: pointer;
                         border-radius: 10px;
